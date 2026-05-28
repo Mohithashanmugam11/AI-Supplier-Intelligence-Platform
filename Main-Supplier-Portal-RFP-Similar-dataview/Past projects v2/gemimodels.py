56d21fdddca730137ee3373a373d9cfc8ef9699a
Mohithashanmugam11 <mohithashanmugam@gmail.com>
2026-05-28 15:00:07 +0530

    Update gemimodels.py

--- Main-Supplier-Portal-RFP-Similar-dataview/Past projects v2/gemimodels.py
+++ Main-Supplier-Portal-RFP-Similar-dataview/Past projects v2/gemimodels.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyBdfpTsqpdL5aVdFVQ4o_kTfWrXlPOZerw")
-#(api_key="AIzaSyDO9Fuz31W50k27Dho-yxPgTN-JBJAdRmk")
+genai.configure(api_key="")
+#(api_key="")
 
 print("Listing available models for your key:")
 for m in genai.list_models():
